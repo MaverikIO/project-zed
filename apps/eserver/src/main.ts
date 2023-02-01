75806df27cc96d22cf7a84c868a02a078bc28301
Maverik Apollo Minett <maverik@maverik.io>
2023-02-01 14:33:23 -0500

    add url logging for all server apps

--- apps/eserver/src/main.ts
+++ apps/eserver/src/main.ts
@@ -1,8 +1,17 @@
 import express from 'express';
 
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+const verbose = true;
 
 const app = express();
+
+/* logging */
+app.use( 
+  (request, response, next ) => {
+    if ( verbose ) console.log( request.url )
+    next()
+  }
+)
 
 app.get('/', (req, res) => {
   res.send({ message: 'Hello API' });
--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,9 +1,14 @@
 import * as http from 'http';
 
 const port = process.env.PORT ? Number(process.env.PORT) : 3001;
+const verbose = true;
 
 http.createServer( 
     ( request, response ) => {
+
+        /* logging */
+        if ( verbose ) console.log( request.url )
+
         response.write('Hello world!')
         response.end()
     },
